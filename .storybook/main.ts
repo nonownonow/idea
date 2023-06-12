d02e0dc14a68f55ec09d905cc140aa09cfa9ebc7
ryuilhwan <ih.ryu.dev@gmail.com>
2023-06-12 16:50:36 +0900

    접근성 테스트와 테스트 커버리지 셋팅

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -14,6 +14,7 @@ const config: StorybookConfig = {
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "@storybook/addon-coverage",
+    "@storybook/addon-a11y",
   ],
   framework: {
     name: "@storybook/react-webpack5",
