b677750a4843ef2d17490b6bcdffd6f2265fded1
ryuilhwan <ih.ryu.dev@gmail.com>
2023-07-23 21:02:40 +0900

    formatting

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,19 +1,34 @@
 import type { StorybookConfig } from "@storybook/react-webpack5";
+
 const config: StorybookConfig = {
-  stories: [{
-    directory: "../packages",
-    files: "**/*.stories.@(js|jsx|ts|tsx)"
-  }, {
-    directory: "../packages",
-    files: "**/*.mdx"
-  }, "../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/addon-coverage", "@storybook/addon-a11y", "@storybook/addon-jest", "@whitespace/storybook-addon-html", "@storybook/addon-mdx-gfm"],
+  stories: [
+    {
+      directory: "../packages",
+      files: "**/*.stories.@(js|jsx|ts|tsx)",
+    },
+    {
+      directory: "../packages",
+      files: "**/*.mdx",
+    },
+    "../stories/**/*.mdx",
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
+  ],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/addon-coverage",
+    "@storybook/addon-a11y",
+    "@storybook/addon-jest",
+    "@whitespace/storybook-addon-html",
+    "@storybook/addon-mdx-gfm",
+  ],
   framework: {
     name: "@storybook/react-webpack5",
-    options: {}
+    options: {},
   },
   docs: {
-    autodocs: "tag"
-  }
+    autodocs: "tag",
+  },
 };
 export default config;
