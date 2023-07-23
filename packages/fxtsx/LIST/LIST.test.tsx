24cd70bd769f94abaa4db58758fdfe44637322c4
ryuilhwan <ih.ryu.dev@gmail.com>
2023-07-23 12:08:04 +0900

    LIST 컴포넌트 구현

--- packages/fxtsx/LIST/LIST.test.tsx
+++ packages/fxtsx/LIST/LIST.test.tsx
@@ -2,16 +2,26 @@ import type { RenderResult } from "@testing-library/react";
 import { render, screen } from "@testing-library/react";
 import { LIST } from "fxtsx/LIST/LIST";
 import { anyPropsWithRootProps } from "fxtsx/FxTsx/FxTsx.test";
-import { ComponentWithoutRef } from "fxtsx/util/util";
+import { ComponentWithoutRef, ComponentWithRef } from "fxtsx/util/util";
+import { createRef } from "react";
 
 describe("랜더링", () => {
   let renderResult: RenderResult;
-  const List = ComponentWithoutRef("ListCallback");
+  const List = ComponentWithRef("ListCallback");
   const Item = ComponentWithoutRef("ItemCallback");
   const data = ["item1", "item2", "item3"];
+  const ref = createRef<HTMLUListElement>();
+  const formatter = (a: any, i: any) => (i === 1 ? `포멧팅한 ${a}` : a);
   beforeEach(() => {
     renderResult = render(
-      <LIST {...anyPropsWithRootProps} List={List} Item={Item} data={data}>
+      <LIST
+        {...anyPropsWithRootProps}
+        List={List}
+        Item={Item}
+        data={data}
+        formatter={formatter}
+        ref={ref}
+      >
         ListRoot
       </LIST>
     );
@@ -22,7 +32,6 @@ describe("랜더링", () => {
       <DocumentFragment>
         <div
           class="my-class"
-          data="item1,item2,item3"
           data-fx-list="true"
           data-test="my-data-test"
           data-testid="ListCallback"
@@ -31,15 +40,35 @@ describe("랜더링", () => {
           style="font-size: 1rem;"
           tab-index="0"
         >
+          <div
+            data-testid="ItemCallback"
+          >
+            item1
+          </div>
+          <div
+            data-testid="ItemCallback"
+          >
+            포멧팅한 item2
+          </div>
+          <div
+            data-testid="ItemCallback"
+          >
+            item3
+          </div>
           ListRoot
         </div>
       </DocumentFragment>
     `);
   });
-  /*  test("프로퍼티 중에서 루트프로퍼티만 루트요소에 전달한다.", () => {
-    expect(screen.getByText("List")).toHaveAttribute("id", "my-id");
-    expect(screen.getByText("List")).not.toHaveAttribute("no-root-props");
-  });*/
+  test("루트프로퍼티를 루트요소에 전달한다.", () => {
+    expect(screen.getByText("ListRoot")).toHaveAttribute("id", "my-id");
+  });
+  test("루트프로퍼티가 아닌 프로퍼티를 루트요소에 전달한다", () => {
+    expect(screen.getByText("ListRoot")).toHaveAttribute(
+      "no-root-props",
+      "my-no-root-props"
+    );
+  });
   test("data-fx-list 속성을 루트요소에 전달한다.", () => {
     expect(screen.getByText("ListRoot")).toHaveAttribute("data-fx-list");
   });
@@ -48,8 +77,20 @@ describe("랜더링", () => {
     test("루트요소는 List 콜백으로 랜더링한다", () => {
       expect(screen.getByTestId("ListCallback")).toHaveTextContent("ListRoot");
     });
-    /*    test("data 프로퍼티로 전달받은 배열 요소를 ITEM 콜백에 프로퍼티로 전달 하여 랜더링한다.", () => {
-      expect(screen.getByText("1"));
-    });*/
+    test("루트요소 List 콜백은 ref 를 받아서 랜더링한다", () => {
+      expect(ref.current).toHaveTextContent("ListRoot");
+    });
+    describe("data 프로퍼티로 배열 요소로 전달받아서", () => {
+      test("ITEM 콜백에 프로퍼티로 전달 하여 랜더링한다.", () => {
+        expect(screen.getAllByTestId("ItemCallback")[0]).toHaveTextContent(
+          "item1"
+        );
+      });
+      test("각 요소를 formatter 프로퍼티로 포멧팅 한다", () => {
+        expect(screen.getAllByTestId("ItemCallback")[1]).toHaveTextContent(
+          "포멧팅한 item2"
+        );
+      });
+    });
   });
 });
--- packages/fxtsx/LIST/LIST.tsx
+++ packages/fxtsx/LIST/LIST.tsx
@@ -1,25 +1,42 @@
-import type { FC, ReactNode } from "react";
+import type { FC, ForwardedRef, Key, ReactNode, Ref } from "react";
 import React from "react";
 import { Fxtsx } from "fxtsx/FxTsx/FxTsx";
-import { ComponentWithoutRef } from "fxtsx/util/util";
+import { ComponentWithoutRef, ComponentWithRef } from "fxtsx/util/util";
 import type { RootElementProps } from "fxtsx/fxtsx.type";
 
-export type LISTProps<T> = LIST<T> & LISTCallback;
-export interface LIST<T> {
+export type LISTProps<T extends Key> = LIST<T> & LISTCallback;
+export interface LIST<T extends Key> {
   data: T[];
+  formatter: (dataElement: T, index: number) => ReactNode;
   children: ReactNode;
 }
 export interface LISTCallback {
-  List?: FC;
-  Item?: FC;
+  List?: FC<{ children: ReactNode; ref: Ref<HTMLUListElement> }>;
+  Item?: FC<{ children: ReactNode }>;
 }
 
-export const MockList = ComponentWithoutRef("List");
+export const MockList = ComponentWithRef("List");
+export const MockItem = ComponentWithoutRef("List");
 export type RestProps<P> = Omit<P, keyof RootElementProps>;
-export const LIST = Fxtsx(function LIST<T>(
+export const LIST = Fxtsx(function LIST<T extends Key>(
   rootProps: RootElementProps,
-  restProps: RestProps<LISTProps<T>>
+  restProps: RestProps<LISTProps<T>>,
+  ref: ForwardedRef<HTMLUListElement>
 ) {
-  const { List = MockList, ...listProps } = restProps;
-  return <List data-fx-list {...rootProps} {...listProps} />;
+  const {
+    List = MockList,
+    Item = MockItem,
+    data,
+    children,
+    formatter,
+    ...listProps
+  } = restProps;
+  return (
+    <List data-fx-list {...rootProps} {...listProps} ref={ref}>
+      {data.map((a, i) => (
+        <Item key={a}>{formatter(a, i)}</Item>
+      ))}
+      {children}
+    </List>
+  );
 });
